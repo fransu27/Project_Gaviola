f95562f231b36ca627767017455bef8ac72a16a7
fransu27 <fygre27@gmail.com>
2026-09-12 10:21:42 +0800

    Fix Prettier code formatting style issues

--- resources/js/components/site-header.tsx
+++ resources/js/components/site-header.tsx
@@ -1,9 +1,7 @@
 export function SiteHeader() {
     return (
         <header className="w-full bg-[#154b1f] text-white">
-            <div className="flex h-[68px] items-center px-6">
-                GOVPH
-            </div>
+            <div className="flex h-[68px] items-center px-6">GOVPH</div>
         </header>
     );
 }
