bd92db399fe7de0220c5895e33d587f91e27e42d
fransu27 <fygre27@gmail.com>
2026-09-12 09:45:46 +0800

    Initial Laravel project

---
+++ resources/js/components/site-header.tsx
@@ -0,0 +1,9 @@
+export function SiteHeader() {
+    return (
+        <header className="w-full bg-[#154b1f] text-white">
+            <div className="flex h-[68px] items-center px-6">
+                GOVPH
+            </div>
+        </header>
+    );
+}
